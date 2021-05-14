1a4d9f3f322c1fdb31829c861f1c92fcf9779561
Zajac P <ptr.zjc@gmail.com>
2021-05-14 20:45:33 +0200

    Initialize SpringBoot repo

---
+++ src/test/java/pl/zajacp/proxy/UserDataProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajacp.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
