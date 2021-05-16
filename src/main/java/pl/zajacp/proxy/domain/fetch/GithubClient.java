6c28383bae3201a92cd61ecb4b068ecb3dd559f3
Zajac P <ptr.zjc@gmail.com>
2021-05-16 14:08:57 +0200

    Create GitHubClient with unit test

---
+++ src/main/java/pl/zajacp/proxy/domain/fetch/GithubClient.java
@@ -0,0 +1,32 @@
+package pl.zajacp.proxy.domain.fetch;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.zajacp.proxy.domain.fetch.model.GithubUserData;
+import pl.zajacp.proxy.infrastructure.api.ExternalPathProperties;
+
+
+interface GithubClient {
+    GithubUserData fetchUserData(String login);
+}
+
+@Slf4j
+@Service
+@AllArgsConstructor
+class GithubClientImpl implements GithubClient {
+
+    private final RestTemplate restTemplate;
+    private final ExternalPathProperties externalPath;
+
+    @Override
+    public GithubUserData fetchUserData(String login) {
+        log.info("Fetching user data for login {}", login);
+        return restTemplate.getForObject(getUserResourceUrl(login), GithubUserData.class);
+    }
+
+    private String getUserResourceUrl(String login){
+        return String.format("/%s/%s", externalPath.getUsersResource(), login);
+    }
+}
---
+++ src/test/groovy/pl/zajacp/proxy/domain/fetch/GithubClientSpec.groovy
@@ -0,0 +1,60 @@
+package pl.zajacp.proxy.domain.fetch
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpStatusCodeException
+import pl.zajacp.proxy.infrastructure.AppConfig
+import pl.zajacp.proxy.infrastructure.api.ApiConfig
+import pl.zajacp.proxy.infrastructure.api.ExternalPathProperties
+import pl.zajacp.proxy.test.config.WiremockConfig
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+@Subject(GithubClient)
+@Narrative("""
+This test check proper response mapping of Github client.
+It is based on defined stubs located under 'wiremock.base-folder' property.
+Logins "ptrzjc" and "octocat" return respose, all other return 404.
+""")
+@AutoConfigureWireMock(port = 9998)
+@SpringBootTest(classes = [GithubClientImpl, WiremockConfig, ApiConfig, ExternalPathProperties, AppConfig])
+class GithubClientSpec extends Specification {
+
+    @Autowired
+    GithubClient client
+
+    @Unroll
+    def "Login #login - check user data fetch"() {
+        when: "User data is fetched"
+        def response = client.fetchUserData(login)
+
+        then: "All fields are mapped correctly"
+        with(response) {
+            it.login.equalsIgnoreCase(login)
+            it.id == id
+            it.name == name
+            it.type == type
+            it.createdAt.toString() == createdAt
+            it.followers == followers
+            it.publicRepos == publicRepos
+            it.avatarUrl.contains("https://avatars.githubusercontent.com/")
+        }
+
+        where:
+        login     || id       | name          | type   | createdAt              | followers | publicRepos
+        "ptrzjc"  || 41077907 | "Piotr"       | "User" | "2018-07-10T17:01:14Z" | 3         | 11
+        "octocat" || 583231   | "The Octocat" | "User" | "2011-01-25T18:44:36Z" | 3735      | 8
+    }
+
+    def "Check data fetch of non-existing user"() {
+        when: "Non existing user data is fetched"
+        client.fetchUserData("Not existing login")
+
+        then: "All fields are mapped correctly"
+        thrown(HttpClientErrorException.NotFound)
+    }
+}
